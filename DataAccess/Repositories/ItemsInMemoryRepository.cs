ae2b64f78abf91b2f8a6f3fd8450d4fe25fea31b
Isaac Calleja <Isaac.calleja.j94845@mcast.edu.mt>
2025-12-06 14:16:10 +0100

    Added ImportItemFactory and In-Memory Repository using Cache, to parse JSON and store temporarily

---
+++ DataAccess/Repositories/ItemsInMemoryRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Interfaces;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace DataAccess.Repositories
+{
+    public class ItemsInMemoryRepository : IItemsRepository
+    {
+        private readonly IMemoryCache _cache;
+        public ItemsInMemoryRepository(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+
+        public void Save(IEnumerable<IItemValidating> items)
+        {
+            // If items is null, we clear the cache
+            if (items == null)
+            {
+                _cache.Remove("BulkImportCache");
+            }
+            else
+            {
+                // Store in RAM for 60 minutes
+                _cache.Set("BulkImportCache", items, TimeSpan.FromMinutes(60));
+            }
+        }
+
+        public IEnumerable<IItemValidating> Get()
+        {
+            _cache.TryGetValue("BulkImportCache", out IEnumerable<IItemValidating> items);
+            return items ?? new List<IItemValidating>();
+        }
+
+        public void Approve(string idString) {}
+    }
+}
---
+++ Presentation/Factory/ImportItemFactory.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Interfaces;
+using Domain.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace Presentation.Factory
+{
+    public class ImportItemFactory
+    {
+        public List<IItemValidating> Create(string json)
+        {
+            var list = new List<IItemValidating>();
+            var jArray = JArray.Parse(json);
+
+            foreach (var token in jArray)
+            {
+                string type = token["type"]?.ToString();
+
+                if (type == "restaurant")
+                {
+                    // Map JSON to Restaurant
+                    var r = token.ToObject<Restaurant>();
+                    // Fix ID: remove "R-" prefix
+                    string rawId = token["id"].ToString();
+                    r.Id = int.Parse(rawId.Replace("R-", ""));
+                    r.Status = "Pending";
+                    list.Add(r);
+                }
+                else if (type == "menuItem")
+                {
+                    var m = token.ToObject<MenuItem>();
+                    m.Id = Guid.NewGuid(); // Generate new Guid per brief
+
+                    // Fix Restaurant ID link
+                    string rawRestId = token["restaurantId"].ToString();
+                    m.RestaurantId = int.Parse(rawRestId.Replace("R-", ""));
+                    m.Status = "Pending";
+                    list.Add(m);
+                }
+            }
+            return list;
+        }
+    }
+}
